dbfedca103ebb914d408f2a6a13a296077509be4
Milanstha16 <sthamilan0000@gmail.com>
2026-08-07 08:24:01 +0545

    Create DatabaseModel & Admin

--- core/Nepalikala/models.py
+++ core/Nepalikala/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.TextChoices):
+    RINGS = "Rings", "Rings"
+    NECKLACES = "Necklaces", "Necklaces"
+    BRACELETS = "Bracelets", "Bracelets"
+    EARRINGS = "Earrings", "Earrings"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    description = models.TextField()
+
+    stock = models.BooleanField(default=True)
+
+    image = models.ImageField(
+        upload_to="products/",
+        blank=True,
+        null=True
+    )
+
+    size = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    material = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    category = models.CharField(
+        max_length=20,
+        choices=Category.choices,
+        default=Category.RINGS
+    )
+
+    def __str__(self):
+        return self.name
